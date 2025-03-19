f9c105ae0f2823453520441fb4e220f12ba60aec
furashcka <furashcka@gmail.com>
2025-03-19 20:43:05 +0300

    Initial configuration of vite

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,26 @@
-import { defineConfig } from 'vite'
+import { resolve } from "path";
+import glob from "glob";
+import { defineConfig } from "vite";
+
+const root = resolve(__dirname, "./");
+const input = glob.sync("./pages/**/*.js");
 
 export default defineConfig({
-  // ...
-})
+  root,
+  publicDir: "./root",
+  resolve: {
+    alias: { "@": root },
+  },
+  server: {
+    origin: "http://localhost:5173",
+    cors: true,
+    strictPort: true,
+  },
+  build: {
+    outDir: "./dist",
+    assetsDir: "assets/template",
+    manifest: "assets/template/manifest.json",
+    rollupOptions: { input },
+  },
+  plugins: [],
+});
