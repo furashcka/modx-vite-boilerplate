7c5e0742717d9e7cdb331e5e151dda1b5f8b2ecf
furashcka <furashcka@gmail.com>
2025-03-26 18:48:47 +0300

    Added hot reload import

--- common/js/base.js
+++ common/js/base.js
@@ -3,3 +3,6 @@ import { instance as dynamicSvgSprite } from "virtual:dynamic-svg-sprite";
 document.addEventListener("DOMContentLoaded", () => {
   dynamicSvgSprite.update(document.body);
 });
+
+// HOT Reload (dev only)
+if (import.meta.hot) import.meta.hot.accept();
